79d36f51f10490658504c06937db93a4d9fe764c
2paperstar <lee@paperst.ar>
2023-12-04 17:09:06 +0900

    chore: add Query component in main page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,9 @@
-import fetchData from '@/api/data';
 import Query from './components/Query';
 
 export default async function Home() {
-  return <main>{/* <Query /> */}</main>;
+  return (
+    <main>
+      <Query />
+    </main>
+  );
 }
