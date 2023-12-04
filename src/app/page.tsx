f1b24a817dc753def47d703b08350f8ad86de8dd
2paperstar <lee@paperst.ar>
2023-12-04 17:02:47 +0900

    feat: separate query component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,20 +1,10 @@
 import fetchData from '@/api/data';
+import Query from './components/Query';
 
 export default async function Home() {
-  const data = await fetchData();
-  const semesters = [
-    ...data.semesters.filter((s) => !s.code),
-    ...[...Array(10)]
-      .map((_, i) => 2023 - i)
-      .flatMap((y) =>
-        data.semesters
-          .filter((s) => s.code)
-          .reverse()
-          .map((s) => ({
-            label: `${y}년도 ${s.label}`,
-            code: `${y}/${s.code}`,
-          })),
-      ),
-  ];
-  return <main>{JSON.stringify(semesters)}</main>;
+  return (
+    <main>
+      <Query />
+    </main>
+  );
 }
