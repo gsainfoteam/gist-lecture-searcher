1e1173f62455253db0d575870795ad138bf4e3ce
2paperstar <lee@paperst.ar>
2023-12-05 02:52:01 +0900

    feat: render lecture table

--- src/app/components/Lectures/index.tsx
+++ src/app/components/Lectures/index.tsx
@@ -24,9 +24,59 @@ const Loader = async ({
     semester: semester.split("/")[1],
   });
   return (
-    <>
-      <div>Query: {JSON.stringify(courses)}</div>
-    </>
+    <section>
+      <table>
+        <thead>
+          <tr>
+            <th>코드</th>
+            <th>교과목명</th>
+            <th>이수</th>
+            <th>담당교수</th>
+            <th>강실학</th>
+            <th>시간표</th>
+            <th>강의실</th>
+            <th>인원</th>
+            <th>언어</th>
+          </tr>
+        </thead>
+        <tbody>
+          {courses.map((course) => (
+            <tr key={course.code}>
+              <td className="whitespace-pre">{course.code}</td>
+              <td>{course.name}</td>
+              <td>{course.creditType}</td>
+              <td>
+                {course.professor
+                  .replace(/\[.+\]/g, "")
+                  .split("\n")
+                  .join(", ")}
+              </td>
+              <td>{course.pnt.replaceAll("/", "")}</td>
+              <td className="whitespace-pre">
+                {Object.entries(
+                  course.timeTable
+                    .split("\n")
+                    .filter(Boolean)
+                    .map((v) => v.split(" "))
+                    .reduce<Record<string, string[]>>(
+                      (prev, curr) => ({
+                        ...prev,
+                        [curr[1]]: [...(prev[curr[1]] ?? []), curr[0]],
+                      }),
+                      {},
+                    ),
+                )
+                  .map(([time, days]) => `${days.join(", ")} ${time}`)
+                  .join("\n")}
+              </td>
+              <td>{course.room}</td>
+              <td className="whitespace-pre">{`${course.count}/${course.capacity}`}</td>
+              <td>{course.language}</td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
+    </section>
   );
 };
 
