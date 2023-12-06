87e633fc192be0e38a088bf157b319a87bd0e84d
2paperstar <lee@paperst.ar>
2023-12-06 19:26:43 +0900

    feat: parse syllabus file

--- src/app/api/syllabuses/route.ts
+++ src/app/api/syllabuses/route.ts
@@ -1,5 +1,35 @@
 import axios from "axios";
+import Excel from "exceljs";
 import { NextRequest, NextResponse } from "next/server";
+
+const clipApi = axios.create({
+  baseURL: "https://zeus.gist.ac.kr/ClipReport4",
+});
+
+const exportOption = (pages: number) => ({
+  exportType: 10,
+  name: "cmVwb3J0",
+  pageType: 1,
+  startNum: 1,
+  endNum: pages,
+  option: {
+    exportMethod: "3",
+    mergeCell: true,
+    mergeEmptyCell: true,
+    splitCellAtPageSize: true,
+    rightToLeft: false,
+    widthRate: 100,
+    heightRate: 100,
+    coordinateErrorLimit: 10,
+    processGerenalFormat: 1,
+    printingMagnification: 100,
+    fitToPageWhenPrinting: false,
+    removeHyperlink: false,
+    repeatPageSectionWhenAllSectionInOneSheet: false,
+    type: "xlsx",
+    splitPage: 0,
+  },
+});
 
 const getUid = async (body: any) => {
   const params = new URLSearchParams();
@@ -12,10 +42,7 @@ const getUid = async (body: any) => {
   params.set("DCLSS_NO", body.classCode);
   params.set("_path", "/uni/uls/UlsOpenSyllabusR");
 
-  const viewerRes = await axios.post(
-    "https://zeus.gist.ac.kr/ClipReport4/reportViewer.jsp",
-    params,
-  );
+  const viewerRes = await clipApi.post("/reportViewer.jsp", params);
   const uid = (viewerRes.data as string).match(/'uid':'([^']*)'/)?.[1];
   return uid;
 };
@@ -24,19 +51,11 @@ const getPages = async (uid: string): Promise<number> => {
   const params = new URLSearchParams();
   params.set("ClipID", "R03");
   params.set("uid", uid);
-  const res = await axios.post(
-    "https://zeus.gist.ac.kr/ClipReport4/Clip.jsp",
-    params,
-  );
-  const resData = JSON.parse(
-    (res.data as string).slice(1, -1).replace(/'/g, '"'),
-  );
-  if (!resData.endReport) {
-    await new Promise((resolve) => setTimeout(resolve, 100));
-    return getPages(uid);
-  }
-  const count = resData.count;
-  return count;
+  const res = await clipApi.post("/Clip.jsp", params);
+  const data = eval(res.data);
+  if (data.endReport) return data.count;
+  await new Promise((resolve) => setTimeout(resolve, 100));
+  return getPages(uid);
 };
 
 const getPage = async (uid: string, pageNumber: number) => {
@@ -46,12 +65,54 @@ const getPage = async (uid: string, pageNumber: number) => {
   params.set("uid", uid);
   params.set("html5", "true");
   params.set("DPI", "96");
-  const res = await axios.post(
-    "https://zeus.gist.ac.kr/ClipReport4/Clip.jsp",
-    params,
-  );
+  const res = await clipApi.post("/Clip.jsp", params);
   const resData = (res.data as string).split("{clipreport_separator}")[1];
   return resData;
+};
+
+const generateFile = async (uid: string, exportOption: object) => {
+  const params = new URLSearchParams();
+  params.set("ClipID", "R09S1");
+  params.set("clipUID", uid);
+  params.set("uid", uid);
+  params.set("path", "/ClipReport4");
+  params.set("optionValue", JSON.stringify(exportOption));
+  params.set("is_ie", "true");
+  params.set("exportN", "cmVwb3J0");
+  params.set("exportType", "10");
+  const res = await clipApi.post("/Clip.jsp", params);
+  const data = eval(res.data);
+  if (!data.status) throw new Error("Failed to generate file");
+  return data;
+};
+
+const waitFile = async (uid: string): Promise<unknown> => {
+  const params = new URLSearchParams();
+  params.set("ClipID", "R09S2");
+  params.set("clipUID", uid);
+  params.set("uid", uid);
+  const res = await clipApi.post("/Clip.jsp", params);
+  const data = eval(res.data);
+  if (data.status) return data;
+  await new Promise((resolve) => setTimeout(resolve, 100));
+  return waitFile(uid);
+};
+
+const getFile = async (uid: string, exportOption: object) => {
+  const params = new URLSearchParams();
+  params.set("ClipID", "R09S3");
+  params.set("uid", uid);
+  params.set("path", "/ClipReport4");
+  params.set("optionValue", JSON.stringify(exportOption));
+  params.set("is_ie", "true");
+  params.set("exportN", "cmVwb3J0");
+  params.set("exportType", "10");
+  const res = await clipApi.post("/Clip.jsp", params, {
+    responseType: "arraybuffer",
+  });
+  const workbook = new Excel.Workbook();
+  await workbook.xlsx.load(res.data);
+  return workbook;
 };
 
 export async function POST(req: NextRequest) {
@@ -64,5 +125,37 @@ export async function POST(req: NextRequest) {
   const pages = await Promise.all(
     [...Array(pageCount)].map(async (_, i) => await getPage(uid, i)),
   );
-  return NextResponse.json({ pages });
+  const option = exportOption(pageCount);
+  await generateFile(uid, option);
+  await waitFile(uid);
+  const wb = await getFile(uid, option);
+  const ws = wb.worksheets[0];
+  const data = {
+    pages,
+    classification: ws.getCell("B4").text,
+    code: ws.getCell("E4").text,
+    pnt: ws.getCell("G4").text,
+    instructor: ws.getCell("I4").text,
+    language: ws.getCell("L4").text,
+    title: {
+      korean: ws.getCell("D5").text,
+      english: ws.getCell("D6").text,
+    },
+    outline: ws.getCell("B7").text,
+    prerequisite: ws.getCell("B8").text,
+    references: ws.getCell("B9").text,
+    lectureMethod: ws.getCell("B10").text,
+    grading: ws.getCell("B11").text,
+    etc: ws.getCell("B12").text,
+    schedules: ws
+      .getRows(15, 20)
+      ?.map((row) => ({
+        week: row.getCell(1).text.trim(),
+        description: row.getCell(2).text,
+        remarks: row.getCell(8).text,
+        onoff: row.getCell(11).text,
+      }))
+      .filter(({ week }) => /^\d{1,2}/.test(week)),
+  };
+  return NextResponse.json(data);
 }
