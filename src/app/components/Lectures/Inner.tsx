2d335d4978de3183714d50a238d2ae3fdcda93f6
2paperstar <lee@paperst.ar>
2023-12-06 12:04:40 +0900

    feat: syllabus loading indicator

--- src/app/components/Lectures/Inner.tsx
+++ src/app/components/Lectures/Inner.tsx
@@ -156,7 +156,7 @@ const Inner = ({ courses, lng }: PropsWithLng<{ courses: Course[] }>) => {
         </table>
       </section>
       <SyllabusesModal
-        pages={syllabuses?.pages}
+        pages={syllabuses?.pages ?? (selectedCourse ? [] : undefined)}
         onClose={() => setSelectedCourse(undefined)}
       />
     </>
@@ -205,7 +205,30 @@ const SyllabusesModal = ({
         onClick={(e) => e.stopPropagation()}
       >
         <div className="animate-bottomUp max-h-full w-max overflow-y-auto rounded-lg bg-white shadow-lg scrollbar-thin scrollbar-track-transparent scrollbar-thumb-neutral-200 scrollbar-thumb-rounded-full dark:bg-neutral-800 dark:scrollbar-thumb-neutral-700">
-          <Syllabuses pages={memoizedPages} />
+          {memoizedPages.length ? (
+            <Syllabuses pages={memoizedPages} />
+          ) : (
+            <svg
+              className="m-4 h-5 w-5 animate-spin text-black dark:text-white"
+              xmlns="http://www.w3.org/2000/svg"
+              fill="none"
+              viewBox="0 0 24 24"
+            >
+              <circle
+                className="opacity-25"
+                cx="12"
+                cy="12"
+                r="10"
+                stroke="currentColor"
+                stroke-width="4"
+              ></circle>
+              <path
+                className="opacity-75"
+                fill="currentColor"
+                d="M4 12a8 8 0 018-8V0C5.373 0 0 5.373 0 12h4zm2 5.291A7.962 7.962 0 014 12H0c0 3.042 1.135 5.824 3 7.938l3-2.647z"
+              ></path>
+            </svg>
+          )}
         </div>
       </div>
     </div>,
