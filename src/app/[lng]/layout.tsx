4fc5c36be1f3456e86fc37d327ee82cc6af1f905
2paperstar <lee@paperst.ar>
2024-01-02 01:08:29 +0900

    chore: add ga

--- src/app/[lng]/layout.tsx
+++ src/app/[lng]/layout.tsx
@@ -3,6 +3,7 @@ import "./globals.css";
 import { Analytics } from "@vercel/analytics/react";
 import type { Metadata } from "next";
 import { Noto_Sans_KR } from "next/font/google";
+import Script from "next/script";
 
 const notoSansKR = Noto_Sans_KR({ subsets: ["latin"] });
 
@@ -18,6 +19,16 @@ export default function RootLayout({
 }) {
   return (
     <html lang="ko">
+      <Script src="https://www.googletagmanager.com/gtag/js?id=G-189NS9CPFD" />
+      <Script id="google-analytics">
+        {`
+          window.dataLayer = window.dataLayer || [];
+          function gtag(){dataLayer.push(arguments);}
+          gtag('js', new Date());
+
+          gtag('config', 'G-189NS9CPFD');
+        `}
+      </Script>
       <body className={notoSansKR.className}>
         {children}
         <Analytics />
